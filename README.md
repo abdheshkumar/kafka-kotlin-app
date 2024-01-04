79ce7c955e44da899090eeff1a6fe5b02bd578b5
abdhesh <abdkumar@expediagroup.com>
2024-01-04 17:44:41 +0000

    Added diagram

--- README.md
+++ README.md
@@ -26,7 +26,15 @@ curl --silent http://localhost:8081/subjects/
 curl --silent http://localhost:8081/subjects/my-topic-avro-value/versions/1/schema
 {"type":"record","name":"User","namespace":"com.user","fields":[{"name":"name","type":"string"},{"name":"age","type":"int"}]}
 ```
+
+#### Kafka, Schema Registry, Kafka Producer and E2E Test
+![Kafka-Avro.png](Kafka-Avro.png)
+
+#### Kafka Streams API
+Kafka Streams provides lightweight APIs to build stream processing applications that process data stored in Kafka using standard streaming concepts and transformation primitives. Using a simple threading model, the streams API avoids the need for back-pressure. This model works well in cases where transformations do not involve external interactions.
+
+Reactor Kafka is useful for streams applications which process data from Kafka and use external interactions (e.g. get additional data for records from a database) for transformations. In this case, Reactor can provide end-to-end non-blocking back-pressure combined with better utilization of resources if all external interactions use the reactive model.
 References:
-1. FOR RESOURCE SAFETY: https://arrow-kt.io/learn/quickstart/
-2. FOR CONFIG: https://github.com/sksamuel/hoplite
+1. For resource safety: https://arrow-kt.io/learn/quickstart/
+2. For config: https://github.com/sksamuel/hoplite
 3. For testing: https://kotest.io/
---
+++ src/main/kotlin/producer/ReactiveKafka.kt
@@ -0,0 +1,75 @@
+package producer
+
+import Config
+import arrow.core.Either
+import com.user.User
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig
+import io.confluent.kafka.serializers.KafkaAvroSerializer
+import loadApplicationConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import reactor.core.Disposable
+import reactor.core.publisher.Flux
+import reactor.kafka.sender.KafkaSender
+import reactor.kafka.sender.SenderOptions
+import reactor.kafka.sender.SenderRecord
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+class ReactiveKafka {
+    private val log: Logger = LoggerFactory.getLogger(javaClass::class.java)
+    private val dateFormat: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss:SSS z dd MMM yyyy")
+    private fun kafkaProperties(config: Config): SenderOptions<String, User> {
+        val props = Properties().apply {
+            put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, config.bootstrapServers)
+            put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java)
+            put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer::class.java)
+            put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, config.schemaRegistryUrl)
+        }
+        return SenderOptions.create(props)
+    }
+
+    private fun createProducer(senderOptions: SenderOptions<String, User>): KafkaSender<String, User> =
+        KafkaSender.create(senderOptions)
+
+    private fun produceMessages(kafkaSender: KafkaSender<String, User>): Disposable {
+        val data = Flux.range(0, 99).map {
+            val user = User.newBuilder().setAge(it).setName("Message-$it").build()
+            val record = ProducerRecord("my-topic-avro", it.toString(), user)
+            SenderRecord.create(record, it)
+        }
+        return kafkaSender.send(data)
+            .doOnError { e -> log.error("Send failed", e) }
+            .subscribe { r ->
+                val metadata = r.recordMetadata()
+                val timestamp = Instant.ofEpochMilli(metadata.timestamp())
+                val message = String.format(
+                    "Message %d sent successfully, topic-partition=%s-%d offset=%d timestamp=%s\n",
+                    r.correlationMetadata(),
+                    metadata.topic(),
+                    metadata.partition(),
+                    metadata.offset(),
+                    dateFormat.format(timestamp),
+                )
+                println(message)
+            }
+    }
+
+    fun startApp(env: String, mapSource: Map<String, Any> = emptyMap()): Disposable {
+        return when (val config: Either<Throwable, Config> = loadApplicationConfig(env, mapSource)) {
+            is Either.Left -> throw config.value
+            is Either.Right -> {
+                val producer = createProducer(kafkaProperties(config.value))
+                produceMessages(producer)
+            }
+        }
+    }
+}
+
+fun main() {
+    ReactiveKafka().startApp("dev")
+}
